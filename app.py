6d5773415c98fb8a310a263f82dc5fa024f594bd
avnms <sarmaakondivnm@icloud.com>
2023-07-29 11:14:05 +0530

    🔤 create form, capture input and save period

--- app.py
+++ app.py
@@ -18,5 +18,34 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
 # --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
-years = [datetime.today().year(), datetime.today().year - 1]
+years = [datetime.today().year, datetime.today().year - 1]
 months = list(calendar.month_name[1:])
+
+
+# --- INPUT & SAVE PERIODS ---
+st.header(f"Data Entry in {currency}")
+with st.form("entry_form", clear_on_submit=True):
+    col_month, col_year = st.columns(2)
+    col_month.selectbox("Select Month:", months, key="month")
+    col_year.selectbox("Select Year:", years, key="year")
+
+    "___"
+    with st.expander("Income"):
+        for income in incomes:
+            st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
+    with st.expander("Expenses"):
+        for expense in expenses:
+            st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+    with st.expander("Comment"):
+        st.text_area("", placeholder="Enter a comment here ...")
+
+    "___"
+    submitted = st.form_submit_button("Save Data")
+    if submitted:
+        period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+        incomes = {income: st.session_state[income] for income in incomes}
+        expenses = {expense: st.session_state[expense] for expense in expenses}
+        # TODO: Insert values into database
+        st.write(f"incomes: {incomes}")
+        st.write(f"expenses: {expenses}")
+        st.success("Data saved!")
