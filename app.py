950f909ce37895d698bb2b8c0a16519062327dfd
avnms <sarmaakondivnm@icloud.com>
2023-07-29 11:25:16 +0530

    📈 Create data visualization form with plot period selection

--- app.py
+++ app.py
@@ -49,3 +49,26 @@ with st.form("entry_form", clear_on_submit=True):
         st.write(f"incomes: {incomes}")
         st.write(f"expenses: {expenses}")
         st.success("Data saved!")
+
+
+# --- PLOT PERIODS ---
+st.header("Data Visualization")
+with st.form("saved_periods"):
+    # TODO: Get periods from database
+    period = st.selectbox("Select Period:", ["2022_January"])
+    submitted = st.form_submit_button("Plot Period")
+    if submitted:
+        # TODO: Get data from database
+        comment = "Some comment"
+        incomes = {"Salary": 100, "Blog": 20, "Other Income": 50}
+        expenses = {"Rent": 10, "Utilities": 10, "Groceries": 10, "Car": 20, "Other Expenses": 30, "Saving": 10}
+
+        # Create metrics
+        total_income = sum(incomes.values())
+        total_expense = sum(expenses.values())
+        remaining_budget = total_income - total_expense
+        col_income, col_expense, col_budget = st.columns(3)
+        col_income.metric("Total Income", f"{total_income} {currency}")
+        col_expense.metric("Total Expense", f"{total_expense} {currency}")
+        col_budget.metric("Remaining Budget", f"{remaining_budget} {currency}")
+        st.text(f"Comment: {comment}")
